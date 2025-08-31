8269f4e37801366d9ad3eb1c03889b3d12f1b7f0
subarnoneel-cse22 <subarnoneel@iut-dhaka.edu>
2025-08-31 20:09:02 +0600

    changes

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "https://<your-firebase-app>.web.app")
 @RestController
 public class HelloController {
 
